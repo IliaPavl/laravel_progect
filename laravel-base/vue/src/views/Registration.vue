4a85b47df9359e33a14c097e5acfccf5a2182a29
IliaPavl <i.pavl@list.ru>
2023-05-02 13:41:29 +0300

    create api for registartration

--- laravel-base/vue/src/views/Registration.vue
+++ laravel-base/vue/src/views/Registration.vue
@@ -8,18 +8,18 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit="registration">
       <div>
-        <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-          >Email address</label
+        <label for="name" class="block text-sm font-medium leading-6 text-gray-900"
+          >Login</label
         >
         <div class="mt-2">
           <input
-            id="email"
-            name="email"
-            type="email"
-            autocomplete="email"
+            id="name"
+            name="name"
+            type="text"
             required=""
+            v-model="user.name"
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
@@ -38,6 +38,7 @@
             type="password"
             autocomplete="current-password"
             required=""
+            v-model="user.password"
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
@@ -55,6 +56,7 @@
             name="passwordRepit"
             type="password"
             required=""
+            v-model="user.password_confirmation"
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
@@ -85,8 +87,24 @@
 
 <style scoped></style>
 
-<script>
+<script setup>
 name: "Registration";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+const user = {
+  name: "",
+  password: "",
+  password_confirmation: "",
+};
+const store = useStore();
+const router = useRouter();
+
+function registration(params) {
+  params.preventDefault();
+  store.dispatch("registartion", user).then((res) => {
+    router.push({ name: "Map" });
+  });
+}
 </script>
 
 <style scoped></style>
