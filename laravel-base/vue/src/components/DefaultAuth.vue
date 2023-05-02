f1a8788e6417bf76e457e27823b5eeace83e5cf3
IliaPavl <i.pavl@list.ru>
2023-05-02 08:48:17 +0300

    clean

--- laravel-base/vue/src/components/DefaultAuth.vue
+++ laravel-base/vue/src/components/DefaultAuth.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-     <router-view></router-view>
-    </div>
-  </template>
-  
-  <script>
-  name: "Login";
-  </script>
-  
-  <style scoped></style>
-  
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+name: "Login";
+</script>
+
+<style scoped></style>
