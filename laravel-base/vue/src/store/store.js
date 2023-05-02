65cc5a598517baf573a7c5d528ba6ea94168e557
IliaPavl <i.pavl@list.ru>
2023-05-02 13:42:20 +0300

    create registartion action

--- laravel-base/vue/src/store/store.js
+++ laravel-base/vue/src/store/store.js
@@ -1,20 +1,48 @@
 import { createStore } from "vuex";
+import axiosClient from "../axios";
 const store = createStore({
     state: {
-        user:{
-            login:"Tom@mail.ru",
-            userId:"fgkf34yytr738qt34crii3",
-            token:'13' 
+        user: {
+            name: null,
+            userId: null,
+            token: sessionStorage.getItem('TOKEN'),
         }
     },
     getters: {},
-    actions: {},
+    actions: {
+        async registartion({ commit }, user) {
+            try {
+                const response = await axiosClient.post('/registration', user, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                commit('setUser', response);
+            } catch (error) {
+                alert(error.response.data.message);
+            }
+        },
+    },
     mutations: {
         logout: (state) => {
-            state.user.name=null;
-            state.user.userId=null;
-            state.user.token=null;
-        }
+            state.user.name = null;
+            state.user.userId = null;
+            state.user.token = null;
+        },
+        setUser: (state, userData) => {
+            const data = userData.data;
+            if (userData !== undefined) {
+                state.user.token = data.token;
+                state.user.name = data.user.name;
+                state.user.userId = data.user.id;
+                sessionStorage.setItem('TOKEN', data.token);
+            } else {
+                alert("Error registration")
+                state.user.token = null;
+                state.user.name = null;
+                state.user.userId = null;
+            }
+        },
     },
     modules: {}
 })
