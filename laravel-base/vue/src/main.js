6e9f3831c00c2eaaef6c5d9e5ea8cf498b59ad6a
IliaPavl <i.pavl@list.ru>
2023-05-03 09:37:56 +0300

    cleaup code

--- laravel-base/vue/src/main.js
+++ laravel-base/vue/src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
-import store  from './store/store'
+import store from './store/store'
 import router from './router/appRouter'
 import App from './App.vue'
 import './input.css'
 
 createApp(App)
-.use(router)
-.use(store)
-.mount('#app')
- 
+    .use(store)
+    .use(router)
+    .mount('#app')
