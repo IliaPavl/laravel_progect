a684ad6a21ebfce108af4b7d9419f6d0d7b77eb8
IliaPavl <i.pavl@list.ru>
2023-05-03 09:39:57 +0300

    add public and auth routs

--- laravel-base/routes/api.php
+++ laravel-base/routes/api.php
@@ -15,8 +15,12 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout',[AuthController::class, 'logout']);
+
 });
-Route::post('/registration',[AuthController::class,'registration']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/registration', [AuthController::class, 'registration']);
+Route::post('/login', [AuthController::class, 'login']);
