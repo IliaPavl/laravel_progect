9009a85ef53663971d7a9aa07b48d975c4b752de
IliaPavl <i.pavl@list.ru>
2023-05-04 17:30:34 +0300

    create modal component

---
+++ laravel-base/vue/src/views/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <TransitionChild
+    as="template"
+    enter="ease-out duration-300"
+    enter-from="opacity-0"
+    enter-to="opacity-100"
+    leave="ease-in duration-200"
+    leave-from="opacity-100"
+    leave-to="opacity-0"
+  >
+    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+  </TransitionChild>
+
+  <div class="fixed inset-0 z-10 overflow-y-auto">
+    <div
+      class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+    >
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        enter-to="opacity-100 translate-y-0 sm:scale-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100 translate-y-0 sm:scale-100"
+        leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      >
+        <DialogPanel
+          class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-10 sm:my-8 sm:w-full sm:max-w-5xl"
+        >
+          <slot name="body"></slot>
+          <div class="bg-white px-5 pb-4 pt-5 sm:p-1 sm:pb-1">
+            <div class="sm:flex sm:items-start">
+              <slot name="bodyText"></slot>
+            </div>
+          </div>
+          <!-- <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto" @click="open = false">Deactivate</button>
+                <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Cancel</button>
+              </div> -->
+        </DialogPanel>
+      </TransitionChild>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { DialogPanel, DialogTitle, TransitionChild } from "@headlessui/vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+</script>
