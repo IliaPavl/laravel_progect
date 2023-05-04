6e41c7a0511a832e8f754009db377feabb347303
IliaPavl <i.pavl@list.ru>
2023-05-04 17:29:19 +0300

    add slot

--- laravel-base/vue/src/components/PageComponent.vue
+++ laravel-base/vue/src/components/PageComponent.vue
@@ -2,13 +2,14 @@
   <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-        {{ title }}
+        <slot name="header"></slot>
       </h1>
+      <slot name="headerSecond"></slot>
     </div>
   </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <slot></slot>
+      <slot name="body"></slot>
     </div>
   </main>
 </template>
@@ -25,9 +26,6 @@ if (user.name === null) {
     name: "Login",
   });
 }
-const props = defineProps({
-  title: String,
-});
 </script>
 
 <style></style>
